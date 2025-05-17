43da1c7759c86d35475eba200d08d731ada587bf
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-05-18 03:50:57 +0600

    Update SwaptwoNumbers.java

--- SwaptwoNumbers.java
+++ SwaptwoNumbers.java
@@ -13,6 +13,7 @@ public class SwapNumbers {
 
         // Value of second is assigned to first
         first = second;
+        
 
         // Value of temporary (which contains the initial value of first) is assigned to second
         second = temporary;
