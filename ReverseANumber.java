c0cfe1064fb2b8754aa84c81cabebef170015a37
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-08-02 00:00:07 +0600

    Update ReverseANumber.java

--- ReverseANumber.java
+++ ReverseANumber.java
@@ -7,7 +7,8 @@ class Main {
 
     // run loop until num becomes 0
     while(num != 0) {
-    
+
+      
       // get last digit from num
       int digit = num % 10;
       reversed = reversed * 10 + digit;
