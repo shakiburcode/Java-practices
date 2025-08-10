440a3c190a4364c4d123ded9d0d89d3d05b6989b
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-08-11 05:20:07 +0600

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -9,6 +9,7 @@ class Main {
     // create an object of Scanner class
     Scanner input = new Scanner(System.in);
 
+    
     // ask users to enter operator
     System.out.println("Choose an operator: +, -, *, or /");
     operator = input.next().charAt(0);
