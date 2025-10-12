04963ff4610a453f33f49614355702082b06bdaa
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-10-13 02:31:00 +0600

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,8 +1,6 @@
 class Main {
   public static void main(String[] args) {
 
-    
-    
     int n = 10, firstTerm = 0, secondTerm = 1;
     System.out.println("Fibonacci Series till " + n + " terms:");
 
